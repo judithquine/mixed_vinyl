8f31b669b04ced9cc522bbb61de6a39e7374b60d
judithquine <judithquine@yahoo.es>
2024-04-25 18:22:12 +0200

    first commit

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -22,7 +22,6 @@ class VinylController extends AbstractController
             ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
         ];
 
-
         return $this->render('vinyl/homepage.html.twig', [
             'title' => 'PB & Jams',
             'tracks' => $tracks,
